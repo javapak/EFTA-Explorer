af462bd81eabfb0ddeb8997f3870fcbdb71d285d
James Pakis <japakis@memphis.edu>
2026-03-11 19:56:06 -0500

    change ip to domain name after setting up certs for https/ssl

--- src/rest-client/AxiosInstance.ts
+++ src/rest-client/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://209.46.121.142', // Replace with your API base URL
+    baseURL: 'https://apaks.dev',
     headers: {
         'Content-Type': 'application/json',
     },
